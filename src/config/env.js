13120a362e0237f12526693830364370fabcfa97
aramadhans <aramadhan@alamisharia.co.id>
2022-12-03 23:49:16 +0700

    update

--- src/config/env.js
+++ src/config/env.js
@@ -3,8 +3,8 @@ const env = {
     process.env.REACT_APP_ENV === "development"
       ? "http://localhost:2022/api/v1"
       : process.env.REACT_APP_ENV === "stagging"
-      ? "http://103.181.183.117:3022/api/v1"
-      : "http://103.181.183.117:4022/api/v1",
+      ? "https://stagging-api-adminpanel.alglodieloggy.com/api/v1"
+      : "https://api-adminpanel.alglodieloggy.com/api/v1",
 };
 
 module.exports = env;
